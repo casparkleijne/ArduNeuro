b35e8e1fcf7c9dcfd958d251bef82ad10f4cc1ad
Caspar Kleijne <caspar.kleijne@hyperdata.nl>
2018-12-20 10:49:32 +0100

    Initial Project Setup

---
+++ ArduNeuro/ArduNeuro.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ArduNeuro.ino
+ Created:	12/20/2018 10:48:49 AM
+ Author:	CasparKleijne
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
